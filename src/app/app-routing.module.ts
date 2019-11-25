2daae1a6cc856c4c42117c55262a2b8b290cd15e
ADEEL3335 <adeel.bsse3335@iiu.edu.pk>
2019-11-25 06:41:54 +0500

    2 commit with storage,login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,6 +14,10 @@ const routes: Routes = [
   {
     path: 'list',
     loadChildren: () => import('./list/list.module').then(m => m.ListPageModule)
+  },
+  {
+    path: 'login',
+    loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
   }
 ];
 
